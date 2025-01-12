f08e7854f24738080adf1428bdea35536c99d7ea
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-01-12 22:08:10 +0530

    welcome

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Digital-Product/' : '', // Corrected repo name
 };
 
