380270dd2b8762ed2cc97250f26035af7bb2fe5c
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-01-18 10:21:31 +0530

    working

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -70,6 +70,9 @@ export default function Contact() {
       className="w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
     ></textarea>
   </div>
+  <input type="hidden" name="_captcha" value="false" />
+
+
 
   <button
     type="submit"
