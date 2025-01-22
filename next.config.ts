2149ef8e2730e3c7aa70de4ae4620698abfb2f5b
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-01-22 13:42:37 +0530

    checking

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Export static HTML/CSS/JS files
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable server-side image optimization
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Digital-Product/' : '', // Corrected repo name
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Digital-Product/' : '', // Use repo name as the prefix for production
 };
 
 module.exports = nextConfig;
