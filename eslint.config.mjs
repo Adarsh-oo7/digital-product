d3551fb6a48e480722ca697b009e3dbda8b6338d
Aromalvg <aromalvijayan448@gmail.com>
2026-06-03 10:43:48 +0530

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,15 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextVitals,
   {
     rules: {
-      "react/no-unescaped-entities": "off",
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
 ];
 
 export default eslintConfig;
